4a377802dca91c759217a2184fe38a76591c863f
Hunter Fuller <hfuller@pixilic.com>
2017-04-11 21:05:18 -0500

    Allow admins to view teams and toggle emphasis.

--- server.py
+++ server.py
@@ -43,11 +43,13 @@ def get_team(id):
 	return json.dumps(teams[int(id)])
 @app.route('/teams/<id>', methods=['POST','PUT'])
 def set_team(id):
+	global teams
 	id = int(id)
 	if 'score' in request.form:
 		teams[id]['score'] = int(request.form['score'])
 	if 'emphasis' in request.form:
 		teams[id]['emphasis'] = (request.form['emphasis'] in ("yes", "true", "True", "checked"))
+		print("Set emphasis to:", teams[id]['emphasis'])
 	return json.dumps(teams[id])
 
 if __name__ == '__main__':
